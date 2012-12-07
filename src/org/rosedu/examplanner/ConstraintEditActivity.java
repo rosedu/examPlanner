7e084836418c82bdc06a993c0b3b6cb530b12cfa
Vlad Bagrin <vlad.bagrin@gmail.com>
2012-12-07 03:03:52 +0200

    Text edit, date picker and a shortcut button to the constraint editor activity

--- src/org/rosedu/examplanner/ConstraintEditActivity.java
+++ src/org/rosedu/examplanner/ConstraintEditActivity.java
@@ -1,7 +1,12 @@
 package org.rosedu.examplanner;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class ConstraintEditActivity extends Activity {
-
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.constraint_edit);
+  }
 }
--- src/org/rosedu/examplanner/LoginActivity.java
+++ src/org/rosedu/examplanner/LoginActivity.java
@@ -1,20 +1,27 @@
 package org.rosedu.examplanner;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 public class LoginActivity extends Activity {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-    }
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_login);
+  }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_login, menu);
-        return true;
-    }
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.activity_login, menu);
+    return true;
+  }
+
+  public void startConstraintEditor(View v) {
+    Intent intent = new Intent(this, ConstraintEditActivity.class);
+    startActivity(intent);
+  }
 }
