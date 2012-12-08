2a4c004989ec268fe47cdfdbaf9a84a27902129e
Flavius Anton <fanton@rosedu.org>
2012-12-08 19:58:06 +0200

    Fix login screen for landscape orientation

--- src/org/rosedu/examplanner/LoginActivity.java
+++ src/org/rosedu/examplanner/LoginActivity.java
@@ -3,6 +3,9 @@ package org.rosedu.examplanner;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends Activity {
 
@@ -16,5 +19,22 @@ public class LoginActivity extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_login, menu);
         return true;
+    }
+    
+    public void userLogin(View v) {
+    	EditText usernameField = (EditText) findViewById(R.id.editText1);
+    	EditText passwordField = (EditText) findViewById(R.id.editText2);
+    	
+    	String username = usernameField.getText().toString();
+    	String password = passwordField.getText().toString();
+    	
+    	SendData(username, password);
+    }
+    
+    public void SendData(String username, String password) {
+    	//TODO: Connect to server and send data
+    	
+    	Toast.makeText(this, "Username: " + username + "\n" 
+    				+ "Password: " + password, Toast.LENGTH_LONG).show();
     }
 }
